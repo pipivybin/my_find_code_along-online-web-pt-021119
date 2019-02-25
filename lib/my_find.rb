5806f54ba50ab685ef12f728aa784f9ecfef8f39
Ming Duan <11203duan@gmail.com>
2019-02-25 22:11:35 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
-
+if collection.class == Fixnum
+  collection if collection % 3 == 0 && collection % 5 == 0
+else
+  i = 0
+  arr = []
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
+
+
+my_find(1) { 1 % 3 == 0 && 1 % 5 == 0 }
